b39196073f2904a35c192a7b1f41c03c0c9a6523
vladostan <vladostan94@gmail.com>
2018-02-16 20:38:01 +0300

    simulation

--- main.m
+++ main.m
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 clc; close; clear;
-% forwardKinematics(5)
-=======
 % Advanced Robotics Manipulation
 % Delta Parallel Robot team assignment
 % Dayanov Nikita, Ostankovich Vladislav, Bortnikov Mikhail, Innopolis 2018
-
 %% Robot model description
-
-
->>>>>>> c31494edf9a4319a80588d50eb54fdac90160773
-
 robot.r = 0.1; % radius of moving platform
 robot.tool_offset = 0; % End-tool offset from moving platform
 robot.R = 0.25; % radius of base platform
